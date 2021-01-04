b4ff9cc72dde41b2904b6ec7ebecf367ce5afc25
WilliamWu <wuwei94@yeah.net>
2021-01-04 23:03:47 +0800

    提交代码

--- Modules/Sample/src/main/java/com/example/william/my/sample/activity/NetStatusActivity.java
+++ Modules/Sample/src/main/java/com/example/william/my/sample/activity/NetStatusActivity.java
@@ -4,6 +4,11 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.william.my.module.activity.ResponseActivity;
 import com.example.william.my.module.router.ARouterPath;
 
+/**
+ * 5.0之前版本我们是通过系统发送的广播来监听的，即ConnectivityManager.CONNECTIVITY_ACTION
+ * 5.0及之后版本我们可以通过ConnectivityManager.NetworkCallback这个类来监听
+ * 7.0及以上静态注册广播不能收到"ConnectivityManager.CONNECTIVITY_ACTION"这个广播了
+ */
 @Route(path = ARouterPath.Sample.Sample_NetStatus)
 public class NetStatusActivity extends ResponseActivity {
 
