6d92888b7500a6f592fafb5880cefe948bb54cca
WilliamWu <wuwei94@yeah.net>
2021-01-15 19:04:05 +0800

    提交代码

--- Modules/Kotlin/src/main/kotlin/com/example/william/my/kotlin/activity/KotlinActivity.kt
+++ Modules/Kotlin/src/main/kotlin/com/example/william/my/kotlin/activity/KotlinActivity.kt
@@ -3,18 +3,26 @@ package com.example.william.my.kotlin.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.example.william.my.kotlin.R
 import com.example.william.my.kotlin.Singleton
+import com.example.william.my.kotlin.databinding.KotlinActivityKotlinBinding
 import com.example.william.my.module.router.ARouterPath
 
+/**
+ * https://developer.android.google.cn/kotlin/ktx
+ */
 @Route(path = ARouterPath.Kotlin.Kotlin)
 class KotlinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.kotlin_activity_kotlin)
 
-        Singleton.getInstance(this).showToast()
+        val binding = KotlinActivityKotlinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.kotlin_activity_kotlin)
+
+        binding.kotlinTextView.setOnClickListener {
+            Singleton.getInstance(this).showToast()
+        }
     }
 
 }
