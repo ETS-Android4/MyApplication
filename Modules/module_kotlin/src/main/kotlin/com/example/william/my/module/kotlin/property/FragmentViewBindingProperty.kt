a99270585abf05f54225502a548a8ae31b54f0ce
WilliamWu <wuwei94@yeah.net>
2021-03-17 19:05:43 +0800

    提交代码

---
+++ Modules/module_kotlin/src/main/kotlin/com/example/william/my/module/kotlin/property/FragmentViewBindingProperty.kt
@@ -0,0 +1,18 @@
+package com.example.william.my.module.kotlin.property
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewbinding.ViewBinding
+
+class FragmentViewBindingProperty<in F : Fragment, out T : ViewBinding>(
+    viewBinder: (F) -> T
+) : LifecycleViewBindingProperty<F, T>(viewBinder) {
+
+    override fun getLifecycleOwner(thisRef: F): LifecycleOwner {
+        try {
+            return thisRef.viewLifecycleOwner
+        } catch (ignored: IllegalStateException) {
+            error("Fragment doesn't have view associated with it or the view has been destroyed")
+        }
+    }
+}
---
+++ Modules/module_kotlin/src/main/kotlin/com/example/william/my/module/kotlin/property/ViewBindingProperty.kt
@@ -0,0 +1,10 @@
+package com.example.william.my.module.kotlin.property
+
+import androidx.annotation.MainThread
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+
+interface ViewBindingProperty<in R : Any, out T : ViewBinding> : ReadOnlyProperty<R, T> {
+    @MainThread
+    fun clear()
+}
