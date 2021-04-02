429c3ae46541196495d9d242b0b26207f35e2c90
WilliamWu <wuwei94@yeah.net>
2021-04-02 10:15:02 +0800

    提交代码

--- librarys/lib_network/src/main/java/com/example/william/my/core/network/retrofit/compat/cookie/CookieJarCompat.java
+++ librarys/lib_network/src/main/java/com/example/william/my/core/network/retrofit/compat/cookie/CookieJarCompat.java
@@ -4,11 +4,6 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
-import com.example.william.my.core.network.retrofit.cookiejar.ClearableCookieJar;
-import com.example.william.my.core.network.retrofit.cookiejar.PersistentCookieJar;
-import com.example.william.my.core.network.retrofit.cookiejar.cache.SetCookieCache;
-import com.example.william.my.core.network.retrofit.cookiejar.persist.SharedPrefsCookiePersist;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,11 +16,9 @@ import okhttp3.OkHttpClient;
 public class CookieJarCompat {
 
     public static void cookieJar(Context context, OkHttpClient.Builder builder) {
-        //builder.addInterceptor(new RetrofitInterceptorCookie());
-        ClearableCookieJar cookieJar = new PersistentCookieJar(
-                new SetCookieCache(), new SharedPrefsCookiePersist(context));
-        //builder.cookieJar(cookieJar);
         builder.cookieJar(buildCookieJar());
+
+        //builder.addInterceptor(new RetrofitInterceptorCookie());
     }
 
     private static CookieJar buildCookieJar() {
