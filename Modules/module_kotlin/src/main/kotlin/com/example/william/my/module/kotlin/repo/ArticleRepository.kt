d01c208320fccfdfd483b90645ee536c3c398dc0
WilliamWu <wuwei94@yeah.net>
2021-04-15 16:40:41 +0800

    提交代码

--- Modules/module_kotlin/src/main/kotlin/com/example/william/my/module/kotlin/repo/ArticleRepository.kt
+++ Modules/module_kotlin/src/main/kotlin/com/example/william/my/module/kotlin/repo/ArticleRepository.kt
@@ -1,14 +1,29 @@
 package com.example.william.my.module.kotlin.repo
 
+import com.example.william.my.core.network.retrofit.utils.RetrofitUtils
 import com.example.william.my.module.bean.ArticleBean
-import com.example.william.my.module.kotlin.source.ArticleDataSource
+import com.example.william.my.module.kotlin.api.KotlinApi
 import com.example.william.my.module.kotlin.utils.ThreadUtils
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 class ArticleRepository {
+
+    private val getArticle: Flow<ArticleBean> = flow {
+
+        //打印线程
+        ThreadUtils.isMainThread("ArticleDataSource getArticle")
+
+        val api = RetrofitUtils.buildApi(KotlinApi::class.java)
+
+        val article = api.getArticle(0)
+        emit(article) // Emits the result of the request to the flow 向数据流发送请求结果
+        delay(3000) // Suspends the coroutine for some time 挂起一段时间
+    }
 
     /**
      * 返回流上的数据转换。
@@ -18,7 +33,7 @@ class ArticleRepository {
      * the current value emitted by the flow at that point in time.
      */
     val article: Flow<ArticleBean> =
-        ArticleDataSource().getArticle
+        getArticle
             // 中间运算符 map 转换数据
             .map { article ->
                 articleTakeOne(article)
