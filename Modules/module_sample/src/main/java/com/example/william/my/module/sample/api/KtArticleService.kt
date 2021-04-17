4ea0b726db5eba324550b0e89a64b2cbfb1311d7
WilliamWu <wuwei94@yeah.net>
2021-04-17 18:17:17 +0800

    提交代码

--- Modules/module_sample/src/main/java/com/example/william/my/module/sample/api/KtArticleService.kt
+++ Modules/module_sample/src/main/java/com/example/william/my/module/sample/api/KtArticleService.kt
@@ -1,7 +1,9 @@
 package com.example.william.my.module.sample.api
 
+import com.example.william.my.core.network.retrofit.response.RetrofitResponse
 import com.example.william.my.module.base.Urls
 import com.example.william.my.module.bean.ArticleBean
+import com.example.william.my.module.bean.ArticleDetailBean
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,4 +11,7 @@ interface KtArticleService {
 
     @GET(Urls.article)
     suspend fun getArticle(@Path("page") page: Int): ArticleBean
+
+    @GET(Urls.article)
+    suspend fun getArticleResponse(@Path("page") page: Int): RetrofitResponse<ArticleDetailBean>
 }
