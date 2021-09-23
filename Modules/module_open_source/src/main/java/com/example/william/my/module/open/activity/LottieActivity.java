c47461b997e246ba219aea873267c590c4a66f50
wuwei <wuwei@weli.cn>
2021-09-23 10:15:06 +0800

    提交代码

--- Modules/module_open_source/src/main/java/com/example/william/my/module/open/activity/LottieActivity.java
+++ Modules/module_open_source/src/main/java/com/example/william/my/module/open/activity/LottieActivity.java
@@ -1,9 +1,13 @@
 package com.example.william.my.module.open.activity;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.william.my.module.open.R;
 import com.example.william.my.module.router.ARouterPath;
@@ -14,9 +18,36 @@ import com.example.william.my.module.router.ARouterPath;
 @Route(path = ARouterPath.OpenSource.OpenSource_Lottie)
 public class LottieActivity extends AppCompatActivity {
 
+    private LottieAnimationView mLottieAnim;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.open_activity_lottie);
+
+        mLottieAnim = findViewById(R.id.lottie);
+        mLottieAnim.addAnimatorListener(
+                new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mLottieAnim.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                }
+        );
     }
 }
